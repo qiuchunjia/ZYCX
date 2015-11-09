d4f1a5ca0d879b3d2820d63f0901e4a5fb20c6b8
qiuchunjia <1465963174@qq.com>
2015-11-09 22:40:54 +0800

    侧滑

---
+++ ZYCX/src/qcjlibrary/fragment/FragmentMenu.java
@@ -0,0 +1,45 @@
+package qcjlibrary.fragment;
+
+import com.zhiyicx.zycx.R;
+
+import android.view.View;
+import qcjlibrary.fragment.base.BaseFragment;
+
+/**
+ * author：qiuchunjia time：下午5:27:29 类描述：这个类是实现
+ *
+ */
+
+public class FragmentMenu extends BaseFragment {
+
+	@Override
+	public void onClick(View v) {
+
+	}
+
+	@Override
+	public void initIntentData() {
+
+	}
+
+	@Override
+	public int getLayoutId() {
+		return R.layout.fragment_slide_menu;
+	}
+
+	@Override
+	public void initView() {
+
+	}
+
+	@Override
+	public void initListener() {
+
+	}
+
+	@Override
+	public void initData() {
+
+	}
+
+}
