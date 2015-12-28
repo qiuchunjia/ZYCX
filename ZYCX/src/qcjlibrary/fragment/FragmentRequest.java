ce2829c2bbcfd05ed28933e13c9630c6e53291cd
CatherineFXX <648129313@qq.com>
2015-12-28 16:46:15 +0800

    开始搜索

---
+++ ZYCX/src/qcjlibrary/fragment/FragmentRequest.java
@@ -0,0 +1,42 @@
+package qcjlibrary.fragment;
+
+import android.view.View;
+import qcjlibrary.fragment.base.BaseFragment;
+
+public class FragmentRequest extends BaseFragment{
+
+	@Override
+	public void onClick(View v) {
+		
+	}
+
+	@Override
+	public void initIntentData() {
+		
+	}
+
+	@Override
+	public int getLayoutId() {
+		// TODO 自动生成的方法存根
+		return 0;
+	}
+
+	@Override
+	public void initView() {
+		// TODO 自动生成的方法存根
+		
+	}
+
+	@Override
+	public void initListener() {
+		// TODO 自动生成的方法存根
+		
+	}
+
+	@Override
+	public void initData() {
+		// TODO 自动生成的方法存根
+		
+	}
+
+}
