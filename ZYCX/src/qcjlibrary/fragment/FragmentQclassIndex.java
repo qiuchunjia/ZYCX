0b1d348caa332fd1d751f8afa20de301ce9ba27d
qiuchunjia <260964739@qq.com>
2016-01-04 11:20:26 +0800

    的

--- ZYCX/src/qcjlibrary/fragment/FragmentQclassIndex.java
+++ ZYCX/src/qcjlibrary/fragment/FragmentQclassIndex.java
@@ -46,12 +46,7 @@ public class FragmentQclassIndex extends BaseFragment {
 	public Object onResponceSuccess(String str, Class class1) {
 		Object object = super.onResponceSuccess(str, class1);
 		if (object instanceof ModelQclass) {
-<<<<<<< HEAD
 			ModelQclass result = (ModelQclass) object;
-=======
-			//手动添加分类推荐
-			ModelQclass result = (ModelQclass)object;
->>>>>>> 438fd8a9b5bfb6ba6fed63c15dba69cbc621bccb
 			ModelQclassCategory recommend = new ModelQclassCategory();
 			recommend.setClass_id(0);
 			recommend.setImooc_class_name("推荐");
@@ -59,8 +54,7 @@ public class FragmentQclassIndex extends BaseFragment {
 			mCategoryList.add(recommend);
 			if (result.getCategory() != null) {
 				mCategoryList.addAll(result.getCategory());
-				QclassFragmentAdapter qAdapter = new QclassFragmentAdapter(
-						getChildFragmentManager(), mCategoryList);
+				QclassFragmentAdapter qAdapter = new QclassFragmentAdapter(getChildFragmentManager(), mCategoryList);
 				vPager.setAdapter(qAdapter);
 				tabpagerIndicator.setViewPager(vPager);
 				tabpagerIndicator.setVisibility(View.VISIBLE);
@@ -76,11 +70,8 @@ public class FragmentQclassIndex extends BaseFragment {
 	@Override
 	public void initData() {
 		mCategoryList = new ArrayList<ModelQclassCategory>();
-<<<<<<< HEAD
-=======
-		//发送请求
+		// 发送请求
 		sendRequest(new api.QclassImpl().index(), ModelQclass.class, 0);
->>>>>>> 438fd8a9b5bfb6ba6fed63c15dba69cbc621bccb
 	}
 
 	@Override
