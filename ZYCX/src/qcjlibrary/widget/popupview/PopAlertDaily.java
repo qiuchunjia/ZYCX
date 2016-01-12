b118d5e3c186652b9871860df256fc8d73909cbe
CatherineFXX <648129313@qq.com>
2016-01-12 17:45:47 +0800

    修改UI

--- ZYCX/src/qcjlibrary/widget/popupview/PopAlertDaily.java
+++ ZYCX/src/qcjlibrary/widget/popupview/PopAlertDaily.java
@@ -9,6 +9,8 @@ import android.app.Activity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
@@ -25,17 +27,24 @@ public class PopAlertDaily extends PopView {
 	 * 类描述：日期提醒弹出框
 	 */
 
-	private TextView tv_alert_everyday;
-	private TextView tv_alert_two;
-	private TextView tv_alert_three;
-	private TextView tv_alert_four;
-	private TextView tv_alert_five;
-	private TextView tv_alert_sex;
-	private TextView tv_alert_seven;
+	private LinearLayout ll_alert_everyday;
+	private LinearLayout ll_alert_two;
+	private LinearLayout ll_alert_three;
+	private LinearLayout ll_alert_four;
+	private LinearLayout ll_alert_five;
+	private LinearLayout ll_alert_sex;
+	private LinearLayout ll_alert_seven;
+	private ImageView iv_alert_everyday;
+	private ImageView iv_alert_two;
+	private ImageView iv_alert_three;
+	private ImageView iv_alert_four;
+	private ImageView iv_alert_five;
+	private ImageView iv_alert_sex;
+	private ImageView iv_alert_seven;
 	private Button btn_alert_check;
 	/** 选中选项的位置**/
 	private int index;
-	private List<TextView> textList;
+	private List<ImageView> imgList;
 	
 	public PopAlertDaily(Activity activity, Object object, 
 			PopResultListener resultListener) {
@@ -49,26 +58,33 @@ public class PopAlertDaily extends PopView {
 
 	@Override
 	public void initPopView() {
-		tv_alert_everyday = (TextView) findViewbyId(R.id.tv_alert_everyday);
-		tv_alert_two = (TextView) findViewbyId(R.id.tv_alert_two);
-		tv_alert_three = (TextView) findViewbyId(R.id.tv_alert_three);
-		tv_alert_four = (TextView) findViewbyId(R.id.tv_alert_four);
-		tv_alert_five = (TextView) findViewbyId(R.id.tv_alert_five);
-		tv_alert_sex = (TextView) findViewbyId(R.id.tv_alert_sex);
-		tv_alert_seven = (TextView) findViewbyId(R.id.tv_alert_seven);
+		ll_alert_everyday = (LinearLayout) findViewbyId(R.id.ll_alert_everyday);
+		ll_alert_two = (LinearLayout) findViewbyId(R.id.ll_alert_two);
+		ll_alert_three = (LinearLayout) findViewbyId(R.id.ll_alert_three);
+		ll_alert_four = (LinearLayout) findViewbyId(R.id.ll_alert_four);
+		ll_alert_five = (LinearLayout) findViewbyId(R.id.ll_alert_five);
+		ll_alert_sex = (LinearLayout) findViewbyId(R.id.ll_alert_sex);
+		ll_alert_seven = (LinearLayout) findViewbyId(R.id.ll_alert_seven);
+		iv_alert_everyday = (ImageView) findViewbyId(R.id.iv_alert_everyday);
+		iv_alert_two = (ImageView) findViewbyId(R.id.iv_alert_two);
+		iv_alert_three = (ImageView) findViewbyId(R.id.iv_alert_three);
+		iv_alert_four = (ImageView) findViewbyId(R.id.iv_alert_four);
+		iv_alert_five = (ImageView) findViewbyId(R.id.iv_alert_five);
+		iv_alert_sex = (ImageView) findViewbyId(R.id.iv_alert_sex);
+		iv_alert_seven = (ImageView) findViewbyId(R.id.iv_alert_seven);
 		btn_alert_check = (Button) findViewbyId(R.id.btn_alert_check);
 	}
 
 	@Override
 	public void initPopData(Object object) {
-		textList = new ArrayList<TextView>();
-		textList.add(tv_alert_everyday);
-		textList.add(tv_alert_two);
-		textList.add(tv_alert_three);
-		textList.add(tv_alert_four);
-		textList.add(tv_alert_five);
-		textList.add(tv_alert_sex);
-		textList.add(tv_alert_seven);
+		imgList = new ArrayList<ImageView>();
+		imgList.add(iv_alert_everyday);
+		imgList.add(iv_alert_two);
+		imgList.add(iv_alert_three);
+		imgList.add(iv_alert_four);
+		imgList.add(iv_alert_five);
+		imgList.add(iv_alert_sex);
+		imgList.add(iv_alert_seven);
 	}
 
 	@Override
@@ -76,59 +92,66 @@ public class PopAlertDaily extends PopView {
 		
 		//监听不同选项
 		//每天
-		tv_alert_everyday.setOnClickListener(new OnClickListener() {
+		ll_alert_everyday.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				index = 0;
+				setDrawableLeft(index);
 			}
 		});
 		//每2天
-		tv_alert_two.setOnClickListener(new OnClickListener() {
+		ll_alert_two.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				index = 1;
+				setDrawableLeft(index);
 			}
 		});
 		//每3天
-		tv_alert_three.setOnClickListener(new OnClickListener() {
+		ll_alert_three.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				index = 2;
+				setDrawableLeft(index);
 			}
 		});
 		//每4天
-		tv_alert_four.setOnClickListener(new OnClickListener() {
+		ll_alert_four.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				index = 3;
+				setDrawableLeft(index);
 			}
 		});
 		//每5天
-		tv_alert_five.setOnClickListener(new OnClickListener() {
+		ll_alert_five.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				index = 4;
+				setDrawableLeft(index);
 			}
 		});
 		//每6天
-		tv_alert_sex.setOnClickListener(new OnClickListener() {
+		ll_alert_sex.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				index = 5;
+				setDrawableLeft(index);
 			}
 		});
 		//每7天
-		tv_alert_seven.setOnClickListener(new OnClickListener() {
+		ll_alert_seven.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				index = 6;
+				setDrawableLeft(index);
 			}
 		});
 		//监听确认按钮
@@ -148,4 +171,13 @@ public class PopAlertDaily extends PopView {
 		});
 	}
 	
+	private void setDrawableLeft(int index){
+		for (int i = 0; i < imgList.size(); i++) {
+			if(i == index){
+				imgList.get(i).setImageResource(R.drawable.xuanzhong);
+			} else{
+				imgList.get(i).setImageResource(R.drawable.weixuanzhong);
+			}
+		}
+	}
 }
